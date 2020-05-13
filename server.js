855ad137a7af3df6b4f51237d651cfd83d69d89d
Ike <Ike@uilleann-pipes-430.dynamic2.rpi.edu>
2020-05-12 20:15:27 -0400

    Added ability for clients to see all current "rooms"
    These are not yet actual socket.io rooms, they are just a json rooms object that contains key value pairs that will in coming updates refer to the {roomname: currently connected clients}

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var rooms = {};
+
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connect', (socket) => {
 	console.log('Socket: ' + socket.id + ' has connected');
+	socket.emit('all-rooms', rooms);
 
 	socket.on('disconnect', () => {
 		console.log('Socket: ' + socket.id + ' has disconnected');
@@ -15,6 +18,13 @@ io.on('connect', (socket) => {
 
 	socket.on('create-room', (roomName) => {
 		console.log(roomName + ' has been created');
+		if(roomName in rooms){
+			console.log("roomName already exists");
+		}else{
+			rooms[roomName] = 1;
+			console.log(rooms);
+			io.emit('all-rooms', rooms);
+		}
 	})
 });
 
