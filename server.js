ef644c7fa6d544865a68fdb07d6ff354b4e61ea6
William Lyons <Lyonsw@rpi.edu>
2020-05-14 16:20:07 -0400

    Added enter username modal and created an online users list.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 var rooms = {};
+var users = [];
+var usersConnected = [];
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,12 +14,36 @@ app.get('/', (req, res) => {
 });
 
 io.on('connect', (socket) => {
+	var uid = null; // https://stackoverflow.com/questions/28930559/handle-browser-refresh-socket-io
 	//upon client connection log the connection and send them the current rooms
 	console.log('Socket: ' + socket.id + ' has connected');
 	socket.emit('update-rooms', rooms);
+	socket.emit('update-users', users);
 
 	socket.on('disconnect', () => {
-		console.log('Socket: ' + socket.id + ' has disconnected');
+		console.log("disconnect triggered");
+		usersConnected.splice(usersConnected.indexOf(uid), 1); //socket always is removed from connected
+
+    	setTimeout(function () {//only remove from users array if disconnect lasts for 10 seconds
+      		if ( usersConnected.indexOf(uid) < 0 ) {
+        		console.log('Socket: ' + socket.id + ' has disconnected');
+        		var index = users.indexOf(uid);
+        		users.splice(index, 1);
+				io.emit('update-users', users);
+      		}
+    	}, 10000);
+	});
+
+	socket.on('new-user', (un) => {
+		if(usersConnected.indexOf(un) < 0){ //reconnect always
+			usersConnected.push(un);
+		}
+
+		if(users.indexOf(un) < 0){ //new user only if the person isnt reconnecting
+			users.push(un);
+			io.emit('update-users', users);
+		}
+		uid = un;
 	});
 
 	socket.on('create-room', (roomName) => {
@@ -27,7 +53,7 @@ io.on('connect', (socket) => {
 			rooms[roomName] = 1;
 			io.emit('update-rooms', rooms);
 		}
-	})
+	});
 });
 
 server.listen(3000, () => {
